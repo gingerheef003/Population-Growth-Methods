1aa5e44f2a7b7be15bf56864dbfb62b267889373
Ginger Heef <gingerheef003@gmail.com>
2021-06-21 17:25:38 +0530

    out of series forecast

--- Time_Series.py
+++ Time_Series.py
@@ -1,8 +1,31 @@
 from pandas import read_csv
+from pandas import to_datetime
+from pandas import DataFrame
+from fbprophet import Prophet
 from matplotlib import pyplot
 
 path = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-car-sales.csv'
 df = read_csv(path, header=0)
 
-df.plot()
+df.columns = ['ds', 'y']
+df['ds']= to_datetime(df['ds'])
+
+model = Prophet()
+model.fit(df)
+
+
+future = list()
+for i in range(1, 13):
+	date = '1969-%02d' % i
+	future.append([date])
+future = DataFrame(future)
+future.columns = ['ds']
+future['ds']= to_datetime(future['ds'])
+
+
+forecast = model.predict(future)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
+
+
+model.plot(forecast)
 pyplot.show()
