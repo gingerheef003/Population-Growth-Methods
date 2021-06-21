154053e415076e0de62b7e3b1e72154fe6c40199
Ginger Heef <gingerheef003@gmail.com>
2021-06-21 17:35:04 +0530

    manually evaluated forecast

--- Time_Series.py
+++ Time_Series.py
@@ -1,6 +1,7 @@
 from pandas import read_csv
 from pandas import to_datetime
 from pandas import DataFrame
+from sklearn.metrics import mean_absolute_error
 from fbprophet import Prophet
 from matplotlib import pyplot
 
@@ -10,8 +11,11 @@ df = read_csv(path, header=0)
 df.columns = ['ds', 'y']
 df['ds']= to_datetime(df['ds'])
 
+train = df.drop(df.index[-12:])
+print(train.tail())
+
 model = Prophet()
-model.fit(df)
+model.fit(train)
 
 
 future = list()
@@ -22,10 +26,16 @@ future = DataFrame(future)
 future.columns = ['ds']
 future['ds']= to_datetime(future['ds'])
 
-
 forecast = model.predict(future)
-print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
 
 
-model.plot(forecast)
+y_true = df['y'][-12:].values
+y_pred = forecast['yhat'].values
+
+mae = mean_absolute_error(y_true,y_pred)
+print('MAE: %.3f' % mae)
+
+pyplot.plot(y_true, label='Actual')
+pyplot.plot(y_pred, label='Predicted')
+pyplot.legend()
 pyplot.show()
